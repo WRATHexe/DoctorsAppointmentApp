98b70a0f4797e13f8ea744a3084784dc6bdb394d
Fardin Rahman <mfardinr@gmail.com>
2025-01-19 14:17:52 +0600

    Pass additional appointment data and disable update button from doctor view
    The changes enable passing additional appointment data, including phone, age, address, gender, and email, to the `AppointmentDetails` activity.
    It also disables the update button if the view is from a doctor.

--- app/src/main/java/cse489/project/doctorsappointmentapp/AppointmentDetails.java
+++ app/src/main/java/cse489/project/doctorsappointmentapp/AppointmentDetails.java
@@ -12,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Objects;
+
 public class AppointmentDetails extends AppCompatActivity {
     private TextView name, phone, age, time, date, address, gender, email, doctorID;
     private ImageView home, appointmentBtn, history;
@@ -92,6 +94,10 @@ public class AppointmentDetails extends AppCompatActivity {
 //            }
 //        });
 
+
+        if(Objects.equals(extras.getString("from"), "doctor")){
+            update.setVisibility(View.GONE);
+        }
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
